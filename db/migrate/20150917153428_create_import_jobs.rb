46da3285025dffa8dedd023cd9b22ef438dc0a25
Fred Duby <fcd1@columbia.edu>
2015-09-17 13:02:41 -0400

    Modify generated migration for import_jobs

--- db/migrate/20150917153428_create_import_jobs.rb
+++ db/migrate/20150917153428_create_import_jobs.rb
@@ -1,6 +1,18 @@
 class CreateImportJobs < ActiveRecord::Migration
+
   def change
+
     create_table :import_jobs do |t|
+
+      t.string :name, null: false
+      t.references :user, index: true
+
+      t.timestamps null: false
+
     end
+
+    add_foreign_key :import_jobs, :users
+
   end
+
 end
