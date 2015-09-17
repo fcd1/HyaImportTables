596235466080fea4d91c40ed17fa399824ea2d51
Fred Duby <fcd1@columbia.edu>
2015-09-17 13:50:45 -0400

    Run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917170306) do
+ActiveRecord::Schema.define(version: 20150917174850) do
 
   create_table "digital_object_imports", force: :cascade do |t|
     t.text     "digital_object_data", limit: 65535
@@ -31,5 +31,9 @@ ActiveRecord::Schema.define(version: 20150917170306) do
   end
 
   add_index "import_jobs", ["user_id"], name: "index_import_jobs_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 255
+  end
 
 end
