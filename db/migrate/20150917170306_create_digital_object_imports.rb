26932da86887501b927ba35a5577027f40170a04
Fred Duby <fcd1@columbia.edu>
2015-09-17 13:27:12 -0400

    Modify generated migration for digital_object_imports

--- db/migrate/20150917170306_create_digital_object_imports.rb
+++ db/migrate/20150917170306_create_digital_object_imports.rb
@@ -1,6 +1,19 @@
 class CreateDigitalObjectImports < ActiveRecord::Migration
+
   def change
+
     create_table :digital_object_imports do |t|
+
+      t.text :digital_object_data
+      t.integer :status, null: false
+      t.references :import_job, null: false, index: true
+
+      t.timestamps null: false
+
     end
+
+    add_foreign_key :digital_object_imports, :import_jobs
+
   end
+
 end
